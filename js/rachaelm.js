284ff7106c5858f14fde3a66fa6c6689132b0d53
rachaelmattison <54870880+rachaelmattison@users.noreply.github.com>
2020-05-31 22:01:27 -0400

    added blm

---
+++ js/rachaelm.js
@@ -0,0 +1,15 @@
+var sketchbookimgs = [
+{
+	"pic" : "<img src=url('"
+}]
+
+// var color = 0;
+// document.onmousemove = function (event) {
+// 	var div = document.createElement("div");
+// 	// could call div whatever i want
+// 	div.style.top = event.y + "px";
+// 	div.style.left = event.x + "px";
+// 	div.style.backgroundColor = "hsl("+ color + ", 100%, 50%)";
+// 	document.body.appendChild(div);
+// 	color = color + 1;
+// }

d93dcc915238fa3295339fbe48329b43255c548a
rachaelmattison <54870880+rachaelmattison@users.noreply.github.com>
2020-06-01 17:52:55 -0400

    added new files and content

--- js/rachaelm.js
+++ js/rachaelm.js
@@ -1,7 +1,114 @@
 var sketchbookimgs = [
 {
-	"pic" : "<img src=url('"
-}]
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499698/large_74255ef9699f2e26691e954118a11153.jpg?1590977861?bc=0'>"
+}, 
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7497485/large_58f5c3fdaafe448df84a5c4353e95c8c.jpg?1590956161?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499697/large_81460f12fac01d063a8f78316b403bcd.jpg?1590977856?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499703/large_78f5b936fc33d4ec25740e47a2e57763.jpg?1590977869?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499736/large_40080242219d29e7f2b11d79f306bf65.png?1590978213?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499737/large_1f36d512ec7a7212839e00256c0cb11d.png?1590978235?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499741/large_b014590af005fa72afee3789584bbd59.png?1590978277?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7499700/large_efe2214274beb92043b832c64fa0ee17.jpg?1590977864?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500129/large_5d595290b686cf9140378187f92dbb3c.jpg?1590982886?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500128/large_61d2ca452e124ce1cf481137885705f8.jpg?1590982885?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500127/large_24d1738e5e2e677209db32224dad8a2b.jpg?1590982885?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500126/large_2192476a7939feec74bebb4d10c7233e.jpg?1590982885?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500125/large_12fcdd08d98a395cb2ac3420f142b9c5.jpg?1590982884?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500124/large_b7abf30fb51e4fafa9a6f9645427fb64.jpg?1590982884?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500123/large_8382124cc8409877d02bb73803f3fdd2.jpg?1590982884?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500122/large_a3e9b410d2230a67f8adcd737e570217.jpg?1590982884?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7500121/large_2b01f24cc06683c9e8753421ecfe2954.jpg?1590982884?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509170/large_4b9b8f4ff2fba6c9b8ae3e1dd3d2ba93.png?1591044860?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509178/large_2aac8e88c47aa7ef83cc8c82506f9406.jpg?1591044893?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509183/large_46d5c71a7d03924ffda8cc4783f2ef0a.jpg?1591044910?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509209/large_0a4f875a98c44144809f948d13ef49d1.jpg?1591044971?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509210/large_906aef550857e39240e057fc8b60b846.jpg?1591044977?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509448/large_3b7966da11478eb740afd4db2311df25.jpg?1591046325?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509449/large_c3912abbad2d346cc5c6788d89437d89.jpg?1591046329?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509698/large_08fd47b6796b0039fbf8b613a24679ea.jpg?1591047701?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509700/large_5d9e2d6d97cd2e727723fa9c7a68c62c.jpg?1591047708?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509806/large_6eefecf261df94bb80a44736aee03799.jpg?1591048049?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509807/large_2750a864924c369ffe81f8180f3d028b.jpg?1591048052?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509808/large_c4feda458e9992fc1f8665e77e02ae08.jpg?1591048053?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509812/large_c14c0a6f792ac2d6151e8811e8f18b07.jpg?1591048057?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509814/large_3e732476feaa360a8cf08cda40b9f594.jpg?1591048059?bc=0'>"
+},
+{
+	"pic" : "<img src='https://d2w9rnfcy7mm78.cloudfront.net/7509817/large_92186427825864869a9c1902e4ae2ed7.jpg?1591048060?bc=0'>"
+}
+]
+
+for (var i=0; i<sketchbookimgs.length; i++) {
+	var skbkimgsDiv = $('<div/>' , {
+		'class' : 'box' + i
+	})
+	.data('sketchbookimgs', sketchbookimgs[i])
+	.html(sketchbookimgs[i].pic)
+	.css({'background-image' : sketchbookimgs[i].pic})
+	.css({'position' : 'sticky'})
+
+	$('.sketchbkimgs').append(skbkimgsDiv)
+}
+
 
 // var color = 0;
 // document.onmousemove = function (event) {
